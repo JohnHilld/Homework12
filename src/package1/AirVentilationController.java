da35a7f5bae91a9c84e14e853cf864997504b52b
JohnHilld <yfdsyfds0@ukr.net>
2023-10-21 02:05:24 +0300

    Homework №12

---
+++ src/package1/AirVentilationController.java
@@ -0,0 +1,49 @@
+package package1;
+
+import java.util.Date;
+
+public class AirVentilationController {
+
+    private int airVentilationLevel;
+    private long systemConnectionTime;
+
+    private AirVentilationController() {
+        airVentilationLevel = 0;
+
+        Date date = new Date();
+        systemConnectionTime = date.getTime();
+    }
+
+    private static class SingleHelper {
+        private static final AirVentilationController INSTANCE = new AirVentilationController();
+    }
+
+    public static AirVentilationController getInstance() {
+        return SingleHelper.INSTANCE;
+    }
+
+    public int getAirVentilationLevel() {
+        return airVentilationLevel;
+    }
+
+    public long getSystemConnectionTime() {
+        return systemConnectionTime;
+    }
+
+    public int increaseVentilation() {
+        if (airVentilationLevel > 100) {
+            return -1;
+        }
+        airVentilationLevel += 1;
+        return airVentilationLevel;
+    }
+
+    public int decreaseVentilation() {
+        if (airVentilationLevel < 0) {
+            return -1;
+        }
+        airVentilationLevel -= 1;
+        return airVentilationLevel;
+    }
+
+}
---
+++ src/package1/Homework12.java
@@ -0,0 +1,41 @@
+package package1;
+
+public class Homework12 {
+
+    public static void main(String[] args) {
+        System.out.println("If you see the same value of \"System start\", then singleton was reused (yay!)" + "\n" +
+                "If you see different values of \"System start\", then 2 singletons were created (booo!!)" + "\n\n" +
+                "RESULT:" + "\n");
+
+        Thread thread1 = new Thread(new ThreadVent1());
+        Thread thread2 = new Thread(new ThreadVent2());
+        Thread thread3 = new Thread(new ThreadVent3());
+        thread1.start();
+        thread2.start();
+        thread3.start();
+    }
+
+    static class ThreadVent1 implements Runnable {
+        @Override
+        public void run() {
+            AirVentilationController vent = AirVentilationController.getInstance();
+            System.out.println("Thread1 [System start " + vent.getSystemConnectionTime() + "] Ventilation level is: " + vent.increaseVentilation());
+        }
+    }
+
+    static class ThreadVent2 implements Runnable {
+        @Override
+        public void run() {
+            AirVentilationController vent = AirVentilationController.getInstance();
+            System.out.println("Thread2 [System start " + vent.getSystemConnectionTime() + "] Ventilation level is: " + vent.increaseVentilation());
+        }
+    }
+
+    static class ThreadVent3 implements Runnable {
+        @Override
+        public void run() {
+            AirVentilationController vent = AirVentilationController.getInstance();
+            System.out.println("Thread3 [System start " + vent.getSystemConnectionTime() + "] Ventilation level is: " + vent.increaseVentilation());
+        }
+    }
+}
